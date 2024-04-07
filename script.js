1d1dfbb0a14b5e593b9feb6f0f7a4f827affc502
avr9570 <134828327+avr9570@users.noreply.github.com>
2024-04-07 15:10:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const quotes = {
     "- Atharva Raut": '"The Way Get Started Is To Quit Talking And Begin Doing."',
-    "- Winston Churchill": '"The Pessimist Sees Difficulty In Every Opportunity. The Optimist Sees Opportunity In Every Difficulty."',
-    "- Will Rogers": '"Don’t Let Yesterday Take Up Too Much Of Today."',
+    "- Anuj Bhagat": '"The Pessimist Sees Difficulty In Every Opportunity. The Optimist Sees Opportunity In Every Difficulty."',
+    "- Sanchi joshi": '"Don’t Let Yesterday Take Up Too Much Of Today."',
     "- Unknown": '"You Learn More From Failure Than From Success. Don’t Let It Stop You. Failure Builds Character."',
-    "- Vince Lombardi": '"It’s Not Whether You Get Knocked Down, It’s Whether You Get Up."',
+    "- Fayez Erum": '"It’s Not Whether You Get Knocked Down, It’s Whether You Get Up."',
     "- Mahatma Gandhi": '"Live as if you were to die tomorrow. Learn as if you were to live forever"',
     "- Martin Luther King Jr": '"Darkness cannot drive out darkness: only light can do that. Hate cannot drive out hate: only love can do that."',
     "- Albert Einstein": '"Strive not to be a success, but rather to be of value."',
